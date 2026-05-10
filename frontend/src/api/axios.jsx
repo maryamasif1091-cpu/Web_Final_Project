b6c6b88fa03c34d731cf55bd5fa368b9a1d919f2
faryalhyder15 <faryalhyder25@gmail.com>
2026-05-10 15:44:18 +0500

    Add Axios API configuration

---
+++ frontend/src/api/axios.jsx
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+const API = axios.create({
+  baseURL: 'http://localhost:5001/api',
+})
+
+API.interceptors.request.use((config) => {
+  try {
+    const user = JSON.parse(localStorage.getItem('user') || '{}')
+    if (user?.token) config.headers.Authorization = `Bearer ${user.token}`
+  } catch {}
+  return config
+})
+
+API.interceptors.response.use(
+  res => res,
+  err => {
+    console.error('API Error:', err.response?.data?.message || err.message)
+    return Promise.reject(err)
+  }
+)
+
+export default API
