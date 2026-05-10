123c220894a55b6f5870b54f4c5f204373dbf6f4
Syeda Faryal Jafferi <faryalhyder25@gmail.com>
2026-05-10 20:15:56 +0500

    Update axios.jsx

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,9 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL 
-    ? `${import.meta.env.VITE_API_URL}/api` 
-    : 'http://localhost:5001/api',
+  baseURL: 'http://localhost:5001/api',
 })
 
 API.interceptors.request.use((config) => {
