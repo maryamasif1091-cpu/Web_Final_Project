44f9bd3eae2b669c3a9ad61ef185441ad0a4f4b8
Maryam <maryamasif1091@gmail.com>
2026-05-10 19:58:11 +0500

    Fix API URL to use environment variable

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: import.meta.env.VITE_API_URL 
+    ? `${import.meta.env.VITE_API_URL}/api` 
+    : 'http://localhost:5001/api',
 })
 
 API.interceptors.request.use((config) => {
